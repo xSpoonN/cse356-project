eddc90b11246b9769de3b10b3bec2c3fe381ccc2
harry <goodgoed@gmail.com>
2024-03-29 18:02:49 -0400

    add log in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,10 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
+  console.log(
+    `${request.method} ${request.url} ${JSON.stringify(request.body)}`
+  );
+
   const response = NextResponse.next();
   response.headers.set('x-cse356', '65b99ec7c9f3cb0d090f2236');
 

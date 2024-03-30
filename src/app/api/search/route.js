f0a811dd265d937f402db1737e6c8e069f41023a
Kevin <kt4cty@gmail.com>
2024-03-29 20:54:15 -0400

    distance calculation

--- src/app/api/search/route.js
+++ src/app/api/search/route.js
@@ -1,14 +1,36 @@
 import { NextResponse } from 'next/server';
 
 function calculateDistance(location, center) {
-  const lonDiff = parseFloat(location.lon) - center[1];
-  const latDiff = parseFloat(location.lat) - center[0];
-  return Math.sqrt(lonDiff * lonDiff + latDiff * latDiff);
+  return cosineDistanceBetweenPoints(
+    parseFloat(center[0]),
+    parseFloat(center[1]),
+    parseFloat(location.lat),
+    parseFloat(location.lon)
+  );
+}
+function cosineDistanceBetweenPoints(lat1, lon1, lat2, lon2) {
+  const R = 6371e3;
+  const p1 = (lat1 * Math.PI) / 180;
+  const p2 = (lat2 * Math.PI) / 180;
+  const deltaP = p2 - p1;
+  const deltaLon = lon2 - lon1;
+  const deltaLambda = (deltaLon * Math.PI) / 180;
+  const a =
+    Math.sin(deltaP / 2) * Math.sin(deltaP / 2) +
+    Math.cos(p1) *
+      Math.cos(p2) *
+      Math.sin(deltaLambda / 2) *
+      Math.sin(deltaLambda / 2);
+  const d = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)) * R;
+  return d;
 }
 export async function POST(request) {
   const { bbox, onlyInBox, searchTerm } = await request.json();
   const { minLat, minLon, maxLat, maxLon } = bbox;
-  const boxCenter = [(minLat + maxLat) / 2, (minLon + maxLon) / 2];
+  const boxCenter = [
+    (parseFloat(minLat) + parseFloat(maxLat)) / 2,
+    (parseFloat(minLon) + parseFloat(maxLon)) / 2,
+  ];
   console.log(
     `POST /search: ${JSON.stringify({ bbox, onlyInBox, searchTerm })}`
   );
@@ -47,20 +69,21 @@ export async function POST(request) {
         return {
           name: row.display_name,
           coordinates: {
-            lon: parseFloat(row.lon),
-            lat: parseFloat(row.lat),
+            lon: row.lon,
+            lat: row.lat,
           },
           bbox: {
-            minLat: parseFloat(minLat),
-            minLon: parseFloat(minLon),
-            maxLat: parseFloat(maxLat),
-            maxLon: parseFloat(maxLon),
+            minLat: minLat,
+            minLon: minLon,
+            maxLat: maxLat,
+            maxLon: maxLon,
           },
           distance: calculateDistance(row, boxCenter),
         };
       });
     }
     res.sort((a, b) => a.distance - b.distance);
+    console.log(res);
     res = res.map(row => {
       delete row.distance;
       return row;

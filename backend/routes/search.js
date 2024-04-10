b00fa0b66cd01113c110d30d437b7605623b21c5
harry <goodgoed@gmail.com>
2024-04-09 20:59:54 -0400

    Without nominatim

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -1,4 +1,17 @@
 const express = require('express');
+const PG = require('pg');
+
+async function connect_db() {
+  const pool = new PG.Pool({
+    user: 'renderer',
+    password: 'renderer',
+    host: 'tile-server',
+    database: 'gis',
+  });
+  const client = await pool.connect();
+
+  return client;
+}
 
 router = express.Router();
 
@@ -39,51 +52,120 @@ router.post('/search', async (req, res) => {
   ];
 
   // Build search query
-  let query = `http://${process.env.BUILD_ENVIRONMENT === 'docker' ? 'nominatim:8080' : 'localhost:9090'}/search?q=${searchTerm}`;
+  let sql = ``;
   if (onlyInBox) {
-    query += `&viewbox=${minLon},${minLat},${maxLon},${maxLat}&bounded=1`;
+    sql = `SELECT 
+        name, 
+        ST_X(ST_Transform(way,4326)) as lon, 
+        ST_Y(ST_Transform(way,4326)) as lat
+      FROM 
+        planet_osm_point 
+      WHERE 
+        LOWER(name) LIKE LOWER('%$1%') AND ST_Transform(ST_MakeEnvelope($2, $3, $4, $5, 4326), 3857) && way
+      UNION
+      SELECT
+        name, 
+        ST_X(ST_Transform(ST_Centroid(ST_Intersection(way, ST_Transform(ST_MakeEnvelope($2, $3, $4, $5, 4326)), 3857))), 4326)) AS lon,
+        ST_Y(ST_Transform(ST_Centroid(ST_Intersection(way, ST_Transform(ST_MakeEnvelope($2, $3, $4, $5, 4326), 3857))), 4326)) AS lat
+      FROM 
+        planet_osm_line 
+      WHERE LOWER(name) LIKE LOWER('%$1%') AND ST_Transform(ST_MakeEnvelope($2, $3, $4, $5, 4326), 3857) && way
+      UNION
+      SELECT 
+        name, 
+        ST_X(ST_Transform(ST_Centroid(ST_Intersection(way, ST_Transform(ST_MakeEnvelope($2, $3, $4, $5, 4326), 3857))), 4326)) AS lon,
+        ST_Y(ST_Transform(ST_Centroid(ST_Intersection(way, ST_Transform(ST_MakeEnvelope($2, $3, $4, $5, 4326), 3857))), 4326)) AS lat
+      FROM
+        planet_osm_polygon 
+      WHERE LOWER(name) LIKE LOWER('%$1%') AND ST_Transform(ST_MakeEnvelope($2, $3, $4, $5, 4326), 3857) && way
+      LIMIT 30;`;
+  } else {
+    sql = `
+      SELECT 
+        name, 
+        ST_X(ST_Transform(way,4326)) as lon, 
+        ST_Y(ST_Transform(way,4326)) as lat,
+        ST_XMin(ST_Envelope(ST_Transform(way,4326))) as minlon,
+        ST_YMin(ST_Envelope(ST_Transform(way,4326))) as minlat,
+        ST_XMax(ST_Envelope(ST_Transform(way,4326))) as maxlon,
+        ST_YMax(ST_Envelope(ST_Transform(way,4326))) as maxlat
+      FROM 
+        planet_osm_point 
+      WHERE
+        LOWER(name) LIKE LOWER(CONCAT('%', $1::text, '%')) 
+      UNION
+      SELECT 
+        name, 
+        ST_X(ST_Centroid(ST_Transform(way,4326))) as lon, 
+        ST_Y(ST_Centroid(ST_Transform(way,4326))) as lat,
+        ST_XMin(ST_Envelope(ST_Transform(way,4326))) as minlon,
+        ST_YMin(ST_Envelope(ST_Transform(way,4326))) as minlat,
+        ST_XMax(ST_Envelope(ST_Transform(way,4326))) as maxlon,
+        ST_YMax(ST_Envelope(ST_Transform(way,4326))) as maxlat
+      FROM 
+        planet_osm_line 
+      WHERE 
+        LOWER(name) LIKE LOWER(CONCAT('%', $1::text, '%')) 
+      UNION
+      SELECT 
+        name, 
+        ST_X(ST_Centroid(ST_Transform(way,4326))) as lon, 
+        ST_Y(ST_Centroid(ST_Transform(way,4326))) as lat,
+        ST_XMin(ST_Envelope(ST_Transform(way,4326))) as minlon,
+        ST_YMin(ST_Envelope(ST_Transform(way,4326))) as minlat,
+        ST_XMax(ST_Envelope(ST_Transform(way,4326))) as maxlon,
+        ST_YMax(ST_Envelope(ST_Transform(way,4326))) as maxlat
+      FROM 
+          planet_osm_polygon
+      WHERE LOWER(name) LIKE LOWER(CONCAT('%', $1::text, '%')) 
+      LIMIT 30;`;
   }
 
   try {
+    const client = await connect_db();
+
     console.log('Sending query to search service');
-    let query_res = await (await fetch(query)).json();
+    let query_res = await client.query(
+      sql,
+      onlyInBox ? [searchTerm, minLon, minLat, maxLon, maxLat] : [searchTerm]
+    );
 
     if (onlyInBox) {
-      query_res = query_res.map(row => {
+      query_res = query_res.rows.map(row => {
         return {
-          name: row.display_name,
+          name: row.name,
           coordinates: {
             lon: parseFloat(row.lon),
             lat: parseFloat(row.lat),
           },
           bbox: {
-            minLat,
-            minLon,
-            maxLat,
-            maxLon,
+            minLat: parseFloat(row.minlat),
+            minLon: parseFloat(row.minlon),
+            maxLat: parseFloat(row.maxlat),
+            maxLon: parseFloat(row.maxlon),
           },
           distance: calculateDistance(row, boxCenter),
         };
       });
     } else {
-      query_res = query_res.map(row => {
-        const [minLat, maxLat, minLon, maxLon] = row.boundingbox;
+      query_res = query_res.rows.map(row => {
         return {
-          name: row.display_name,
+          name: row.name,
           coordinates: {
             lon: parseFloat(row.lon),
             lat: parseFloat(row.lat),
           },
           bbox: {
-            minLat: parseFloat(minLat),
-            minLon: parseFloat(minLon),
-            maxLat: parseFloat(maxLat),
-            maxLon: parseFloat(maxLon),
+            minLat: parseFloat(row.minlat),
+            minLon: parseFloat(row.minlon),
+            maxLat: parseFloat(row.maxlat),
+            maxLon: parseFloat(row.maxlon),
           },
           distance: calculateDistance(row, boxCenter),
         };
       });
     }
+
     query_res.sort((a, b) => a.distance - b.distance);
 
     query_res = query_res.map(row => {
@@ -91,12 +173,12 @@ router.post('/search', async (req, res) => {
       return row;
     });
 
-    console.debug(`Query result: ${JSON.stringify(query_res)}`);
+    client.release();
 
     return res.status(200).json(query_res);
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json(err);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
   }
 });
 

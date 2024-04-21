46c9b5de1ae609c2eb4cbe8d7d454bf3ee757cb9
harry <goodgoed@gmail.com>
2024-04-20 22:46:34 -0400

    search service port changed & setting different image tag

--- script/push-images-to-registry.sh
+++ script/push-images-to-registry.sh
@@ -2,10 +2,11 @@
 docker login
 REMOTE_REGISTRY="ktao87/cse356"
 IMAGES=("cse356_project-tile-server" "cse356_project-db" "cse356_project-search" "cse356_project-frontend" "cse356_project-backend" "cse356_project-fluentd" "cse356_project-nginx")
+TAG="prod"
 
 # Loop through each image and push it to the remote registry after tagging with the registry address
 for image in "${IMAGES[@]}"; do
-    echo "Pushing $image to $REMOTE_REGISTRY"
+    echo "Pushing $image:$TAG to $REMOTE_REGISTRY"
     docker tag "$image" "$REMOTE_REGISTRY:$image"
     docker push "$REMOTE_REGISTRY:$image"
     docker rmi "$REMOTE_REGISTRY:$image"

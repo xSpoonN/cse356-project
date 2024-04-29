6c46a0636a8c49732dedbd608b2efae5693f48dc
harry <goodgoed@gmail.com>
2024-04-29 13:40:27 -0400

    Add init script and minor fixes

--- docker/tile-server/tile-entrypoint.sh
+++ docker/tile-server/tile-entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+echo "Waiting for db to start..."
+while ! PGPASSWORD=mysecretpassword psql -h db -U postgres -c "SELECT 1" > /dev/null; do
+  sleep 10
+done
+
 echo "Starting apache2 and renderd"
 service apache2 restart
-G_MESSAGES_DEBUG=all renderd -f &
+G_MESSAGES_DEBUG=info renderd -f &
 # tail -F /var/log/renderd.log /var/log/apache2/access.log /var/log/apache2/error.log &
 pid=${!}
-
-# render_list_geo.pl -t "memcached://tile-cache:11211" -x -77.55 -X -71.06 -y 40.31 -Y 44.2 -z 0 -Z 13 -n 4
 
 wait $pid

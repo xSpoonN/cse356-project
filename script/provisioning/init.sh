2832c0ba47497143f795e2f15b2db3df8f902e42
harry <goodgoed@gmail.com>
2024-04-29 17:18:06 -0400

    FIX: platform is not supported in docker swarm

--- script/provisioning/init.sh
+++ script/provisioning/init.sh
@@ -7,7 +7,7 @@ TILE_HOST=$2
 
 java -jar ./fdt.jar -ss 1M -P 10 -r -c $DB_HOST ../../backup/ -d /
 
-ssh -i cse356 root@$DB_HOST 'docker container run -p 5432:5432 -v osm-data:/var/lib/postgresql/14/main -v /backup:/backup --tmpfs /dev/shm:size=2048000000 -d ktao87/cse356:db'
+ssh -i cse356 root@$DB_HOST 'docker container run --name db -p 5432:5432 -v osm-data:/var/lib/postgresql/14/main -v /backup:/backup --tmpfs /dev/shm:size=2048000000 -n -d ktao87/cse356:db'
 
 java -jar ./fdt.jar -ss 1M -P 10 -c $TILE_HOST ../../tile-server-cache.tar.gz -d /var/cache/tiles
 
@@ -17,5 +17,7 @@ ssh -i cse356 root@$TILE_HOST 'tar -xvf /var/cache/tiles/tile-server-cache.tar.g
 while ! docker container run --rm -e PGPASSWORD=mysecretpassword postgres:14-bullseye psql -h $DB_HOST -U postgres -d gis -c "SELECT 1" > /dev/null; do
   sleep 60
 done
+
+ssh -i cse356 root@$DB_HOST 'docker container rm -f db'
 
 echo "EVERYTHING IS READY. CREATE MORE SERVERS AND CONTINUE TO DEPLOY THE SERVICE by running -- ansible-playbook -i ./inventory.ini ./deploy.yml!"

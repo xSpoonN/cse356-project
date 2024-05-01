f37e471b103eef8a6bccff713e06628b7396d59a
harry <goodgoed@gmail.com>
2024-05-01 11:31:07 -0400

    cache post request

--- backend/routes/routing.js
+++ backend/routes/routing.js
@@ -80,7 +80,10 @@ router.post('/route', async (req, res) => {
     console.debug('Query Result: ', noDuplicates);
 
     client.release();
-    return res.status(200).json(noDuplicates);
+    return res
+      .set('Cache-Control', 'public, max-age=7200')
+      .status(200)
+      .json(noDuplicates);
   } catch (error) {
     console.error('Error executing query', error);
     client.release();
@@ -88,64 +91,64 @@ router.post('/route', async (req, res) => {
   }
 });
 
-router.post('/route/full', async (req, res) => {
-  const { source, destination } = req.body;
-  const { lat: srcLat, lon: srcLon } = source;
-  const { lat: dstLat, lon: dstLon } = destination;
-  const client = await connect_db();
-  const sql = `
-  WITH source AS (
-    SELECT source AS source_id
-    FROM us_northeast_2po_4pgr
-    ORDER BY geom_way <-> ST_SetSRID(ST_MakePoint($2, $1), 4326)
-    LIMIT 1
-  ),
-  destination AS (
-    SELECT target AS target_id
-    FROM us_northeast_2po_4pgr
-    ORDER BY geom_way <-> ST_SetSRID(ST_MakePoint($4, $3), 4326)
-    LIMIT 1
-  ),
-  route AS (
-    SELECT * FROM pgr_dijkstra(
-      'SELECT id, source, target, cost, reverse_cost FROM us_northeast_2po_4pgr',
-      (SELECT source_id FROM source),
-      (SELECT target_id FROM destination),
-      FALSE
-    )
-  )
-  SELECT
-    r.seq, r.node, r.edge, r.cost, r.agg_cost, 
-    ST_Length(w.geom_way::geography) AS distance,
-    ST_AsGeoJSON(w.geom_way) AS geomjson,
-    ST_X(ST_StartPoint(w.geom_way)) AS start_lon,
-    ST_Y(ST_StartPoint(w.geom_way)) AS start_lat,
-	  ST_X(ST_EndPoint(w.geom_way)) AS end_lon,
-    ST_Y(ST_EndPoint(w.geom_way)) AS end_lat
-  FROM route r
-  JOIN us_northeast_2po_4pgr w ON r.edge = w.id;
-  `;
+// router.post('/route/full', async (req, res) => {
+//   const { source, destination } = req.body;
+//   const { lat: srcLat, lon: srcLon } = source;
+//   const { lat: dstLat, lon: dstLon } = destination;
+//   const client = await connect_db();
+//   const sql = `
+//   WITH source AS (
+//     SELECT source AS source_id
+//     FROM us_northeast_2po_4pgr
+//     ORDER BY geom_way <-> ST_SetSRID(ST_MakePoint($2, $1), 4326)
+//     LIMIT 1
+//   ),
+//   destination AS (
+//     SELECT target AS target_id
+//     FROM us_northeast_2po_4pgr
+//     ORDER BY geom_way <-> ST_SetSRID(ST_MakePoint($4, $3), 4326)
+//     LIMIT 1
+//   ),
+//   route AS (
+//     SELECT * FROM pgr_dijkstra(
+//       'SELECT id, source, target, cost, reverse_cost FROM us_northeast_2po_4pgr',
+//       (SELECT source_id FROM source),
+//       (SELECT target_id FROM destination),
+//       FALSE
+//     )
+//   )
+//   SELECT
+//     r.seq, r.node, r.edge, r.cost, r.agg_cost,
+//     ST_Length(w.geom_way::geography) AS distance,
+//     ST_AsGeoJSON(w.geom_way) AS geomjson,
+//     ST_X(ST_StartPoint(w.geom_way)) AS start_lon,
+//     ST_Y(ST_StartPoint(w.geom_way)) AS start_lat,
+// 	  ST_X(ST_EndPoint(w.geom_way)) AS end_lon,
+//     ST_Y(ST_EndPoint(w.geom_way)) AS end_lat
+//   FROM route r
+//   JOIN us_northeast_2po_4pgr w ON r.edge = w.id;
+//   `;
 
-  try {
-    const query_res = await client.query(sql, [srcLat, srcLon, dstLat, dstLon]);
-    const route = query_res.rows.map(row => ({
-      description: `Step ${row.seq} (${row.name}): Move to node ${row.node} via edge ${row.edge}. Cost: ${row.cost}, Aggregate cost: ${row.agg_cost}`,
-      edge: [
-        [row.start_lon, row.start_lat],
-        [row.end_lon, row.end_lat],
-      ],
-      geoJson: JSON.parse(row.geomjson),
-      distance: row.distance,
-    }));
+//   try {
+//     const query_res = await client.query(sql, [srcLat, srcLon, dstLat, dstLon]);
+//     const route = query_res.rows.map(row => ({
+//       description: `Step ${row.seq} (${row.name}): Move to node ${row.node} via edge ${row.edge}. Cost: ${row.cost}, Aggregate cost: ${row.agg_cost}`,
+//       edge: [
+//         [row.start_lon, row.start_lat],
+//         [row.end_lon, row.end_lat],
+//       ],
+//       geoJson: JSON.parse(row.geomjson),
+//       distance: row.distance,
+//     }));
 
-    client.release();
-    res.setHeaders('Cache-Control', 'public, max-age=7200');
-    return res.status(200).json(route);
-  } catch (error) {
-    console.error('Error executing query', error);
-    client.release();
-    return res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+//     client.release();
+//     res.setHeaders('Cache-Control', 'public, max-age=7200');
+//     return res.status(200).json(route);
+//   } catch (error) {
+//     console.error('Error executing query', error);
+//     client.release();
+//     return res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
 
 module.exports = router;

d8c93e437b57cee05788c765753256a3e1e5ad0a
harry <goodgoed@gmail.com>
2024-05-01 05:35:02 -0400

    deploy with nginx cache

--- script/provisioning/init.sh
+++ script/provisioning/init.sh
@@ -3,15 +3,15 @@
 set -e
 
 DB_HOST=$1
-TILE_HOST=$2
+MAIN_HOST=$2
 
 java -jar ./fdt.jar -ss 1M -P 10 -r -c $DB_HOST ../../backup/ -d /
 
 ssh -i cse356 root@$DB_HOST 'docker container run --name db -p 5432:5432 -v osm-data:/var/lib/postgresql/14/main -v /backup:/backup --tmpfs /dev/shm:size=2048000000 -d ktao87/cse356:db'
 
-java -jar ./fdt.jar -ss 1M -P 10 -c $TILE_HOST ../../tile-server-cache.tar.gz -d /var/cache/tiles/
+java -jar ./fdt.jar -ss 1M -P 10 -c $MAIN_HOST ../../nginx.tar.gz -d /
 
-ssh -i cse356 root@$TILE_HOST 'tar -xvf /var/cache/tiles/tile-server-cache.tar.gz -C /var/cache/tiles && mv /var/cache/tiles/data/default /var/cache/tiles/default && rmdir /var/cache/tiles/data && touch -d "1 Jan" /var/cache/tiles/default/planet-import-complete'
+ssh -i cse356 root@$MAIN_HOST 'mkdir /var/cache/tiles && tar -xvf /nginx-cache.tar.gz -C /var/cache/tiles --strip-components 1 && chmod -R 777 /var/cache/tiles'
 
 # Try to connect to the database
 while ! docker container run --rm -e PGPASSWORD=mysecretpassword postgres:14-bullseye psql -h $DB_HOST -U postgres -d gis -c "SELECT 1" > /dev/null; do

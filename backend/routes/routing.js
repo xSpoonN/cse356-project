e6fa5a38a9b9ffb515d8d01f71db011ca664eb29
harry <goodgoed@gmail.com>
2024-05-03 00:48:38 -0400

    Routing cache

--- backend/routes/routing.js
+++ backend/routes/routing.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const Memcached = require('memcached');
+const util = require('util');
 const PG = require('pg');
 
 async function connect_db() {
@@ -14,6 +16,9 @@ async function connect_db() {
 }
 
 router = express.Router();
+const memcached = new Memcached('route-cache:11211');
+memcached.get = util.promisify(memcached.get);
+memcached.set = util.promisify(memcached.set);
 
 // router.use((req, res, next) => {
 //   if (!req.session.username) {
@@ -25,40 +30,105 @@ router = express.Router();
 // });
 
 router.post('/route', async (req, res) => {
+  function round_number(num) {
+    return Math.round(num * 10) / 10;
+  }
+
   console.log('Received /route request');
 
   const { source, destination } = req.body;
-  const { lat: srcLat, lon: srcLon } = source;
-  const { lat: dstLat, lon: dstLon } = destination;
+  let { lat: srcLat, lon: srcLon } = source;
+  let { lat: dstLat, lon: dstLon } = destination;
 
-  const client = await connect_db();
-  const sql = `
-  WITH source AS (
-    SELECT source AS source_id
-    FROM us_northeast_2po_4pgr
-    ORDER BY geom_way <-> ST_SetSRID(ST_MakePoint($2, $1), 4326)
-    LIMIT 1
-  ),
-  destination AS (
-    SELECT target AS target_id
-    FROM us_northeast_2po_4pgr
-    ORDER BY geom_way <-> ST_SetSRID(ST_MakePoint($4, $3), 4326)
-    LIMIT 1
-  )
+  // Do rounding to 1 decimal places
+  srcLat = round_number(srcLat);
+  srcLon = round_number(srcLon);
+  dstLat = round_number(dstLat);
+  dstLon = round_number(dstLon);
+
+  // Check if source to destination key exists in cache
+  let cacheKey = `${srcLat},${srcLon},${dstLat},${dstLon}`;
+  let data;
+  try {
+    console.log('Checking cache: ', cacheKey);
+    data = await memcached.get(cacheKey);
+  } catch (err) {
+    console.error('Error fetching from cache', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+
+  let source_id, target_id;
+  if (!data) {
+    console.log('Cache miss');
+    // If the key does not exist in cache, execute the query to find the node_id
+    const client = await connect_db();
+    let sql = `
+      WITH source AS (
+        SELECT source AS source_id
+        FROM us_northeast_2po_4pgr
+        ORDER BY geom_way <-> ST_SetSRID(ST_MakePoint($2, $1), 4326)
+        LIMIT 1
+      ),
+      destination AS (
+        SELECT source AS target_id
+        FROM us_northeast_2po_4pgr
+        ORDER BY geom_way <-> ST_SetSRID(ST_MakePoint($4, $3), 4326)
+        LIMIT 1
+      )
+      SELECT source_id, target_id FROM source, destination;
+    `;
+    let { rows } = await client.query(sql, [srcLat, srcLon, dstLat, dstLon]);
+    console.log('Query executed successfully');
+    client.release();
+    source_id = rows[0].source_id;
+    target_id = rows[0].target_id;
+    memcached.set(cacheKey, `${source_id},${target_id}`, 14400);
+    cacheKey = `${source_id},${target_id}`;
+  } else {
+    cacheKey = data;
+  }
+
+  // Check if the source to destination key exists in cache
+  try {
+    data = await memcached.get(cacheKey);
+  } catch (err) {
+    console.error('Error fetching from cache', err);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+
+  // If the key exists in cache, return the route
+  if (data) {
+    console.log('Cache hit');
+    return res
+      .set('Cache-Control', 'public, max-age=14400')
+      .status(200)
+      .json(data);
+  }
+
+  // If the key does not exist in cache, execute the query to find the route
+  sql = `
   SELECT *
   FROM pgr_dijkstra(
-  'SELECT id, source, target, cost, reverse_cost 
-  FROM us_northeast_2po_4pgr as e,
-  (SELECT ST_Expand(ST_Extent(geom_way),0.1) as box FROM us_northeast_2po_4pgr as b
-    WHERE b.source = '|| (SELECT source_id FROM source) ||' OR b.source = ' || (SELECT target_id FROM destination) || ') as box WHERE e.geom_way && box.box',
-  array(SELECT source_id FROM source),
-  array(SELECT target_id FROM destination),
-  FALSE) AS di
+    format('
+      SELECT id, source, target, cost, reverse_cost
+      FROM us_northeast_2po_4pgr as e,
+      (
+        SELECT ST_Expand(ST_Extent(geom_way),0.1) as box
+        FROM us_northeast_2po_4pgr as b
+        WHERE b.source = %L OR b.source = %L
+      ) as box
+      WHERE e.geom_way && box.box',
+      $1::integer, $2::integer
+    ),
+    array[$1::integer], array[$2::integer], FALSE
+  ) AS di
   JOIN us_northeast_2po_4pgr AS w ON di.edge = w.id;
-  `;
+`;
   try {
     console.log('Executing query');
-    const query_res = await client.query(sql, [srcLat, srcLon, dstLat, dstLon]);
+    const client = await connect_db();
+    const query_res = await client.query(sql, [source_id, target_id]);
+    console.log('Query executed successfully');
     const route = query_res.rows.map(row => ({
       description: `Step ${row.seq} (${row.osm_name}): Turn ${row.turn_direction} Move to node ${row.node} via edge ${row.edge}. Distance: ${row.distance}, Cost: ${row.cost}, Aggregate cost: ${row.agg_cost}`,
       coordinates: {
@@ -77,11 +147,11 @@ router.post('/route', async (req, res) => {
         })
       );
     });
-    console.debug('Query Result: ', noDuplicates);
+    memcached.set(cacheKey, noDuplicates, 14400);
 
     client.release();
     return res
-      .set('Cache-Control', 'public, max-age=7200')
+      .set('Cache-Control', 'public, max-age=14400')
       .status(200)
       .json(noDuplicates);
   } catch (error) {
